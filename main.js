653d8da38571b63511af893f4c22229e60984556
DaRon Burgess <darondburgess@yahoo.com>
2023-06-06 02:28:27 -0400

    Donut click, auto click max 4, still need interval

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ makeDonuts.onclick = function() {
     count += 1;
     makeDonuts.innerHTML = "Donuts Made: " + count;
 };
+
+var autoClicker = document.getElementById("auto-clicker"),
+autoCount = 0;
+autoCountMax = 3;
+autoClicker.onclick = function() {
+    if(autoCount <= autoCountMax){
+    autoCount += 1;
+    autoClicker.innerHTML = "Auto Clickers Owned: " + autoCount;
+        setInterval(() => {
+            makeDonuts.click();
+            count++;
+        }, 1000);
+    }
+};
