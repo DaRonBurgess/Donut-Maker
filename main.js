bb52f5d51ba34de45b9d86eb62684581c752c687
DaRon Burgess <darondburgess@yahoo.com>
2023-06-06 21:17:21 -0400

    broke code, trying to understand, got back

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-var makeDonuts = document.getElementById("make-donuts"),
+var makeDonuts = document.getElementById('make-donuts'),
 count = 0;
 makeDonuts.onclick = function() {
     count += 1;
-    makeDonuts.innerHTML = "Donuts Made: " + count;
+    makeDonuts.innerHTML = 'Donuts Made: ' + count;
 };
 
-var autoClicker = document.getElementById("auto-clicker"),
+var autoClicker = document.getElementById('auto-clicker'),
 autoCount = 0;
 autoCountMax = 3;
 autoClicker.onclick = function() {
     if(autoCount <= autoCountMax){
     autoCount += 1;
-    autoClicker.innerHTML = "Auto Clickers Owned: " + autoCount;
+    autoClicker.innerHTML = 'Auto Clickers Owned: ' + autoCount;
         setInterval(() => {
             makeDonuts.click();
             count ++;
